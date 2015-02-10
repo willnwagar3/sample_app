cb5cb50a01641c59f6f1384bd8bb352e1bfb9762
William Wagar <willnwagar3@icloud.com>
2015-02-10 17:19:32 -0500

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_select "title", "Ruby on Rails Sample Tutorial"
+  end
+
+end
